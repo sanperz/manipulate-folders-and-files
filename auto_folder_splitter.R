017712a8bd538d0a70959fbe0e03157a50a0552b
Sandra Perez <perez.sandra.c@gmail.com>
2025-10-25 19:03:40 -0400

    Update auto_folder_splitter.R

--- auto_folder_splitter.R
+++ auto_folder_splitter.R
@@ -11,23 +11,24 @@ num_batches <- ceiling(length(all_files) / batch_size)
 
 # CREATE SUBFOLDERS AND DISTRIBUTE FILES #######################################
 for (i in 1:num_batches) {
-  # DEFINE THE BATCH FOLDER NAME ###############################################
+  # DEFINE THE BATCH FOLDER NAME #
   batch_folder <- file.path(main_folder, paste0(output_folder_prefix, i))
   
-  # CREATE THE BATCH FOLDER ####################################################
+  # CREATE THE BATCH FOLDER #
   if (!dir.exists(batch_folder)) {
     dir.create(batch_folder)
   }
   
-  # DETERMINE THE FILES FOR THIS BATCH #########################################
+  # DETERMINE THE FILES FOR THIS BATCH #
   start_idx <- ((i - 1) * batch_size) + 1
   end_idx <- min(i * batch_size, length(all_files))
   batch_files <- all_files[start_idx:end_idx]
   
-  # MOVE FILES TO BATCH FOLDER #################################################
+  # MOVE FILES TO BATCH FOLDER #
   file.rename(batch_files, file.path(batch_folder, basename(batch_files)))
   
   message(paste("Moved", length(batch_files), "files to", batch_folder))
 }
 
 message("Folder splitting completed.")
+
