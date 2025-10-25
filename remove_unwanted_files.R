99f2dea1e352e7143a1e7f56132a6e1fd8d5dfc1
Sandra Perez <perez.sandra.c@gmail.com>
2025-10-25 19:02:59 -0400

    Update remove_unwanted_files.R

--- remove_unwanted_files.R
+++ remove_unwanted_files.R
@@ -1,24 +1,24 @@
 install.packages("readxl", dependencies = TRUE)
 library(readxl)
 
-# DEFINE PATHS
-# Change this to the path with list of files to remove
-excel_file <- "C:/Users/Sandra/OneDrive/Desktop/CE Data Sept 25/CE Files to Remove.xlsx"
+# DEFINE PATHS #################################################################
+# Change this to the path with list of files to remove #
+excel_file <- "C:/Users/ *ADD PATH TO XLSX FILE THAT CONTAINS LIST OF FILES TO REMOVE* "
 
-# Change this to the folder path that will be cleaned
-folder_path <- "C:/Users/Sandra/OneDrive/Desktop/CE Data Sept 25/Cleaned Data"
+# Change this to the folder path that will be cleaned #
+folder_path <- "C:/Users/ *ADD PATH TO FOLDER THAT CONTAINS FILES TO BE REMOVED* "
 
-# READ EXCEL FILE
+# READ EXCEL FILE ##############################################################
 unwanted_files <- read_excel(excel_file, col_names = TRUE)
 
-# Assuming the filenames are in the first column
+# Assuming the filenames are in the first column #
 file_names <- unwanted_files[[1]]
 
-# LOOP THROUGH EACH FILE AND DELETE UNWANTED ONES
+# LOOP THROUGH EACH FILE AND DELETE UNWANTED ONES ##############################
 for (file_name in file_names) {
   file_path <- file.path(folder_path, file_name)
   
-  # Check if file exists before attempting to delete
+  # CHECK IF FILE EXISTS BEFORE ATTEMPTING TO DELETE #
   if (file.exists(file_path)) {
     file.remove(file_path)
     message(paste("Deleted:", file_path))
@@ -26,5 +26,6 @@ for (file_name in file_names) {
     message(paste("File not found:", file_path))
   }
 }
+
 
 message("File cleanup completed.")
